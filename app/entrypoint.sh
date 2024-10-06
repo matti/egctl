586b2a7f8653681df2d8d86224275adf935de9f9
Matti Paksula <matti.paksula@iki.fi>
2024-10-06 21:16:36 +0300

    stream

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -9,13 +9,19 @@ if [[ -z "$EGCTL_STATES" ]]; then
   EGCTL_STATES=${*:2}
 fi
 
+if [[ -z "$EGCTL_SILENT" ]]; then
+  stream=/dev/null
+else
+  stream=/dev/stdout
+fi
+
 echo "default ${EGCTL_PROTOCOL:-pms21} $EGCTL_IP ${EGCTL_PORT:-5000} ${EGCTL_PASSWORD:-1}" > /etc/egtab
 
 if [[ "${EGCTL_LOOP:-}" == "yes" ]]; then
   while true
   do
-    echo "$(date) - setting states ${EGCTL_STATES}"
-    egctl default $EGCTL_STATES || echo "egctl failed"
+    >"$stream" 2>&1 echo "$(date) - setting states ${EGCTL_STATES}"
+    >"$stream" 2>&1 egctl default $EGCTL_STATES || echo "egctl failed"
     sleep "${EGCTL_INTERVAL:-5}"
   done
 else
