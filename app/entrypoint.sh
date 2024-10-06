5de291c2a1d6fda0edde7254f8dcc167bf6a27c0
Matti Paksula <matti.paksula@iki.fi>
2024-10-06 20:26:06 +0300

    LOOP

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -eEuo pipefail
 
 EGCTL_IP=$1
 EGCTL_STATES=${*:2}
 
 echo "default ${EGCTL_PROTOCOL:-pms21} $EGCTL_IP ${EGCTL_PORT:-5000} ${EGCTL_PASSWORD:-1}" > /etc/egtab
 
-exec egctl default $EGCTL_STATES
+if [[ "${EGCTL_LOOP:-}" == "yes" ]]; then
+  while true
+  do
+    egctl default $EGCTL_STATES || echo "egctl failed"
+    sleep "${EGCTL_INTERVAL:-5}"
+  done
+else
+  exec egctl default $EGCTL_STATES
+fi
